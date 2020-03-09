033f7296bb46721276f5e1efa8a2d1c8e92e1be9
Chibuzor Obiora <cobiora@getcarbon.co>
2020-03-09 20:16:25 +0100

    Add api tests and mocks

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,1 +1,26 @@
-describe("API tests", () => {});
+const CoindeskAPI = require("../src/datasources/coindeskapi");
+const mocks = require("./mocks");
+
+describe.only("BuyCoins API", () => {
+  it("should correctly return the resolved computed value", async () => {
+    const coindeskAPI = new CoindeskAPI();
+    const type = "buy";
+    const margin = 0.2;
+    const exchangeRate = 365;
+
+    // mock API call
+    jest
+      .spyOn(coindeskAPI, "get")
+      .mockImplementation(() => Promise.resolve(mocks.getNgnMockResponse));
+
+    const actual = await coindeskAPI.getCalculatedPrice(
+      type,
+      margin,
+      exchangeRate
+    );
+    const expected = { nairaValue: 3041181.477009 };
+
+    expect(coindeskAPI.get).toHaveBeenCalled();
+    expect(expected).toEqual(actual);
+  });
+});
--- tests/mocks.js
+++ tests/mocks.js
@@ -1,2 +1,20 @@
-const mocks = {};
-module.exports = mocks;
+const mocks = {
+    getNgnMockResponse: JSON.stringify({
+      bpi: {
+        USD: {
+          code: "USD",
+          rate: "8,315.3733",
+          description: "United States Dollar",
+          rate_float: 8315.3733
+        },
+        NGN: {
+          code: "NGN",
+          rate: "3,039,268.9533",
+          description: "Nigerian Naira",
+          rate_float: 3039268.9533
+        }
+      }
+    })
+  };
+
+  module.exports = mocks;
